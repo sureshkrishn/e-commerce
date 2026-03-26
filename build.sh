5139225fbc3dc055587d7ed9b5777cddf33a9d68
root <root@ip-172-31-39-150.ec2.internal>
2026-03-26 11:17:24 +0000

    updated-files and jenkin-install file

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-IMAGE_NAME="sureshkrishn/dev:latest"
+ENV=$1
 
-echo "🚀 Building Docker image..."
+if [ "$ENV" == "prod" ]; then
+  IMAGE_NAME="sureshkrishn/prod:latest"
+else
+  IMAGE_NAME="sureshkrishn/dev:latest"
+fi
+
+echo "🚀 Building Docker image for $ENV..."
+
 docker build -t $IMAGE_NAME .
+
+if [ $? -ne 0 ]; then
+  echo "❌ Build failed"
+  exit 1
+fi
 
 echo "📤 Pushing image to Docker Hub..."
 docker push $IMAGE_NAME
 
-echo "✅ Build & Push completed"
+if [ $? -ne 0 ]; then
+  echo "❌ Push failed"
+  exit 1
+fi
+
+echo "✅ Image pushed successfully: $IMAGE_NAME"
